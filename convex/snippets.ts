118cf4b2e219db41669a57a512d18cf33e7b88ff
Soumik Majumder <soumikcode02@gmail.com>
2024-12-27 23:59:05 +0530

    snippets page, search, star or delete snippet, with issnippetstarred, getsnippetstarcount, deletesnippet, starsnippet query and mutation

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values"
-import { mutation } from "./_generated/server"
+import { mutation, query } from "./_generated/server"
 
 export const createSnippet = mutation({
     args: { title: v.string(), language: v.string(), code: v.string() },
@@ -23,5 +23,117 @@ export const createSnippet = mutation({
         })
 
         return snippetId;
+    }
+})
+
+export const getSnippets = query({
+    handler: async(ctx) => {
+        const snippets = await ctx.db.query("snippets").order("desc").collect();
+        return snippets;
+    }
+})
+
+export const isSnippetStarred = query({
+    args: { snippetId: v.id("snippets") },
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            return false;
+        }
+
+        const star = await ctx.db.query("stars").withIndex("by_user_id_and_snippet_id").filter((q) => q.eq(q.field("userId"), identity.subject) && q.eq(q.field("snippetId"), args.snippetId)).first();
+
+        return !!star;
+    }
+})
+
+export const getSnippetStarCount = query({
+    args: { snippetId: v.id("snippets") },
+    handler: async(ctx, args) => {
+        const stars = await ctx.db.query("stars").withIndex("by_snippet_id").filter((q) => q.eq(q.field("snippetId"), args.snippetId)).collect();
+
+        return stars.length;
+    }
+})
+
+export const deleteSnippet = mutation({
+    args: { snippetId: v.id("snippets") },
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new ConvexError("Not authenticated");
+        }
+
+        const [user, snippet] = await Promise.all([
+            ctx.db.query("users").withIndex("by_user_id").filter((q) => q.eq(q.field("userId"), identity.subject)).first(),
+            ctx.db.query("snippets").filter((q) => q.eq(q.field("_id"), args.snippetId)).first()
+        ])
+        
+        if (!user) {
+            throw new ConvexError("User not found");
+        }
+
+        if (!snippet) {
+            throw new ConvexError("Snippet not found");
+        }
+
+        if (snippet.userId != identity.subject) {
+            throw new ConvexError("You cannot delete this snippet");
+        }
+        
+        const [comments, stars] = await Promise.all([
+            ctx.db.query("snippetComments").withIndex("by_snippet_id").filter((q) => q.eq(q.field("snippetId"), args.snippetId)).collect(),
+            ctx.db.query("stars").withIndex("by_snippet_id").filter((q) => q.eq(q.field("snippetId"), args.snippetId)).collect()
+        ])
+
+        for (const comment of comments)
+        {
+            await ctx.db.delete(comment._id);
+        }
+
+        for (const star of stars)
+        {
+            await ctx.db.delete(star._id);
+        }
+
+        await ctx.db.delete(snippet._id);
+    }
+})
+
+export const starSnippet = mutation({
+    args: { snippetId: v.id("snippets") },
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new ConvexError("Not authenticated");
+        }
+
+        const [user, snippet, star] = await Promise.all([
+            ctx.db.query("users").withIndex("by_user_id").filter((q) => q.eq(q.field("userId"), identity.subject)).first(),
+            ctx.db.query("snippets").filter((q) => q.eq(q.field("_id"), args.snippetId)).first(),
+            ctx.db.query("stars").withIndex("by_user_id_and_snippet_id").filter((q) => q.eq(q.field("userId"), identity.subject) && q.eq(q.field("snippetId"), args.snippetId)).first()
+        ])
+
+        if (!user) {
+            throw new ConvexError("User not found");
+        }
+
+        if (!snippet) {
+            throw new ConvexError("Snippet not found");
+        }
+
+        if (star) {
+            await ctx.db.delete(star._id);
+        }
+        else
+        {
+            await ctx.db.insert("stars", {
+                userId: user._id,
+                snippetId: args.snippetId
+            })
+        }
     }
 })
---
+++ src/components/StarButton.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useAuth } from '@clerk/nextjs'
+import { Id } from '../../convex/_generated/dataModel'
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import toast from 'react-hot-toast';
+import { Star } from 'lucide-react';
+
+function StarButton({ snippetId }: { snippetId: Id<"snippets"> }) {
+  const isSignedIn = useAuth();
+  const isStarred = useQuery(api.snippets.isSnippetStarred, { snippetId });
+  const starCount = useQuery(api.snippets.getSnippetStarCount, { snippetId });
+  const starSnippet = useMutation(api.snippets.starSnippet);
+
+  const cls1 = "bg-yellow-500/10 text-yellow-500 hover:bg-yellow-500/20";
+  const cls2 = "bg-gray-500/10 text-gray-400 hover:bg-gray-500/20";
+
+  async function handleStar()
+  {
+    if (!isSignedIn) {
+        return;
+    }
+
+    try {
+        await starSnippet({ snippetId });
+    }
+    catch(err: any) {
+        console.log("error while starring snippet");
+        toast.error(err.message);
+    }
+  }
+  
+  return (
+    <button className={`group flex items-center justify-center gap-1.5 px-3 py-1.5 rounded-lg transition-all duration-200
+    ${isStarred? cls1 : cls2}`} onClick={handleStar} >
+      <Star className={`w-4 h-4 ${isStarred ? "fill-yellow-500" : "fill-none group-hover:fill-gray-400"}`} />
+
+      <span className={`text-xs font-medium ${isStarred ? "text-yellow-500" : "text-gray-400"} text-center`}>
+        {starCount}
+      </span>
+    </button>
+  )
+}
+
+export default StarButton
